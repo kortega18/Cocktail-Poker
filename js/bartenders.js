7045832d08cadc339119c774c6e7f0e001ff42f7
Kassandra Ortega <49657598+kortega18@users.noreply.github.com>
2023-11-02 03:21:38 -0500

    Add files via upload

---
+++ js/bartenders.js
@@ -0,0 +1,146 @@
+let button_name;
+let color_choice;
+let disableButton;
+
+
+let kassie_button = document.getElementById('kassies-button');
+kassie_button.onclick = function() {
+  button_name = 'kassie';
+}
+
+let yessenia_button = document.getElementById('yessenias-button');
+yessenia_button.onclick = function() {
+  button_name = 'yessenia';
+}
+
+let almasa_button = document.getElementById('almasas-button');
+almasa_button.onclick = function() {
+  button_name = 'almasa';
+}
+
+let amanda_button = document.getElementById('amandas-button');
+amanda_button.onclick = function() {
+  button_name = 'amanda';
+}
+
+let kate_button = document.getElementById('kates-button');
+kate_button.onclick = function() {
+  button_name = 'kate';
+}
+
+let kimberly_button = document.getElementById('kimberlys-button');
+kimberly_button.onclick = function() {
+  button_name = 'kimberly';
+}
+
+// let mariana_button = document.getElementById('marianas-button');
+// mariana_button.onclick = function() {
+//   button_name = 'mariana';
+// }
+
+let yuri_button = document.getElementById('yuris-button');
+yuri_button.onclick = function() {
+  button_name = 'yuri';
+}
+
+let lily_button = document.getElementById('lilys-button');
+lily_button.onclick = function() {
+  button_name = 'lily';
+}
+
+
+
+
+let black_CT = document.getElementById("blackCT-button");
+black_CT.onclick = function() {
+  color_choice = 'Black Cocktail';
+  disableButton = 'blackCT-button';
+}
+
+let white_CT = document.getElementById("whiteCT-button");
+white_CT.onclick = function() {
+  color_choice = 'White Cocktail';
+  disableButton = 'whiteCT-button';
+
+}
+
+let pink_CT = document.getElementById("pinkCT-button");
+pink_CT.onclick = function() {
+  color_choice = 'Pink Cocktail';
+  disableButton = 'pinkCT-button';
+
+}
+
+let red_CT = document.getElementById("redCT-button");
+red_CT.onclick = function() {
+  color_choice = 'Red Cocktail';
+  disableButton = 'redCT-button';
+
+}
+
+let orange_CT = document.getElementById("orangeCT-button");
+orange_CT.onclick = function() {
+  color_choice = 'Orange Cocktail';
+  disableButton = 'orangeCT-button';
+
+}
+
+let yellow_CT = document.getElementById("yellowCT-button");
+yellow_CT.onclick = function() {
+  color_choice = 'Yellow Cocktail';
+  disableButton = 'yellowCT-button';
+
+}
+
+let green_CT = document.getElementById("greenCT-button");
+green_CT.onclick = function() {
+  color_choice = 'Green Cocktail';
+  disableButton = 'greenCT-button';
+
+}
+
+let blue_CT = document.getElementById("blueCT-button");
+blue_CT.onclick = function() {
+  color_choice = 'Blue Cocktail';
+  disableButton = 'blueCT-button';
+
+}
+
+let purple_CT = document.getElementById("purpleCT-button");
+purple_CT.onclick = function() {
+  color_choice = 'Purple Cocktail';
+  disableButton = 'purpleCT-button';
+
+}
+
+
+
+
+let submit_button = document.getElementById('submit-button');
+submit_button.onclick = function() {
+    var drink_name = document.getElementById('text-area-drink').value;
+    var thought = document.getElementById('text-area-thought').value;
+
+    var img_location = "img/bartenders/" + button_name + ".jpg";
+    var span_heading = drink_name + ' - ' + color_choice;
+
+    var img_to_change = button_name + '-img';
+    var name_to_change = button_name + '-name';
+    var span_to_change = button_name + '-span';
+    var p_to_change = button_name + '-p';
+
+    document.getElementById(disableButton).disabled = true;
+
+    // console.log(drink_name);
+    // console.log(thought);
+    // console.log(span_heading);
+    // console.log(color_choice);
+    // console.log(button_name);
+
+    localStorage.setItem(img_to_change, img_location);
+    localStorage.setItem(name_to_change, button_name);
+    localStorage.setItem(span_to_change, span_heading);
+    localStorage.setItem(p_to_change, thought);
+
+    window.location.href = "index.html";
+}
---
+++ js/colors.js
@@ -0,0 +1,21 @@
+var app = angular.module('myApp', []);
+
+app.controller('AppCtrl', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
+
+// Load the data
+$http.get('http://www.corsproxy.com/loripsum.net/api/plaintext').then(function (res) {
+  $scope.loremIpsum = res.data;
+  $timeout(expand, 0);
+});
+
+$scope.autoExpand = function(e) {
+      var element = typeof e === 'object' ? e.target : document.getElementById(e);
+      var scrollHeight = element.scrollHeight -30; // replace 60 by the sum of padding-top and padding-bottom
+      element.style.height =  scrollHeight + "px";    
+  };
+
+function expand() {
+  $scope.autoExpand('text-area-drink');
+  $scope.autoExpand('text-area-thought');
+}
+}]);
